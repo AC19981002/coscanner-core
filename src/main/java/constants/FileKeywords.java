334a9cdd269a69adcfbe629ee751546d18305e1c
chenzhen08 <chenzhen08@kuaishou.com>
2022-07-05 10:31:17 +0800

    [coscanner][add] add enums

--- src/main/java/constants/FileKeywords.java
+++ src/main/java/constants/FileKeywords.java
@@ -5,6 +5,8 @@ package constants;
  * @date 2022/7/5 10:27 上午
  * @Email 1351170669@qq.com
  */
-//TODO a set of prefix and suffix
+//TODO
+// a set of prefix and suffix
+// like .java .properties or .class...
 public enum FileKeywords {
 }
